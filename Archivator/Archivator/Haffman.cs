5a7899ac0de3ce7e35bac1941ca03342a2a59f6a
Anton Yaskov <yaskovanton333@gmail.com>
2024-10-31 23:56:37 +0500

    Added Haffman alg

---
+++ Archivator/Archivator/Haffman.cs
@@ -0,0 +1,105 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Archivator;
+
+public static class Haffman
+{
+    public static void Decompress(string dictFilePath, string compressedFilePath, string resultFilePath)
+    {
+        var dict = JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(dictFilePath))!
+            .ToDictionary(e => e.Value, e => e.Key);
+        var bytes = File.ReadAllBytes(compressedFilePath)
+            .Select(e => Convert.ToString(e, 2))
+            .ToArray();
+        var bytesAsString = bytes
+            .Take(bytes.Length - 1)
+            .Select(ToFullByte)
+            .Concat(new[] {bytes.Last()});
+        var str = string.Join("", bytesAsString);
+
+        using var writer = new StreamWriter(resultFilePath);
+        var curCh = "";
+        foreach (var ch in str)
+        {
+            curCh += ch;
+            if (dict.TryGetValue(curCh, out var decodedCh))
+            {
+                writer.Write(decodedCh);
+                curCh = "";
+            }
+        }
+    }
+
+    private static string ToFullByte(string source)
+    {
+        if (source.Length == 8)
+            return source;
+
+        return new string('0', 8 - source.Length) + source;
+    }
+
+    public static void Compress(string filePath, string dictFilePath, string resultFilePath)
+    {
+        var text = File.ReadAllText(filePath);
+        var countByCh = text
+            .GroupBy(e => e)
+            .Select(e => new { ch = e.Key, count = e.Count()})
+            .OrderByDescending(e => e.count)
+            .ToDictionary(e => e.ch, e => e.count);
+        var codesByCh = GetCodes(countByCh);
+        File.WriteAllText(dictFilePath, JsonSerializer.Serialize(codesByCh));
+        
+        using var resultFileStream = File.Open(resultFilePath, FileMode.OpenOrCreate);
+        using var writer = new BinaryWriter(resultFileStream, Encoding.Default);
+        var bytes = string.Join("", text.Select(e => codesByCh[e]))
+            .Chunk(8)
+            .Select(e => new string(e))
+            .Select(e => Convert.ToByte(e, 2))
+            .ToArray();
+        writer.Write(bytes);
+    }
+
+    private static Dictionary<char, string> GetCodes(Dictionary<char, int> countPerCodes)
+    {
+        var dictElementsCount = countPerCodes.Count;
+        var treeDepth = 1;
+        while (Math.Pow(2, treeDepth) < dictElementsCount)
+        {
+            treeDepth++;
+        }
+
+        return countPerCodes.Select((e, i) => new {ch = e.Key, code = GetCode(i, treeDepth)})
+            .ToDictionary(e => e.ch, e => e.code);
+    }
+    
+    private static StringBuilder cur = new("");
+    private static string GetCode(int ind, int treeDepth)
+    {
+        if (ind == 0)
+        {
+            cur = new StringBuilder(new string('0', treeDepth));
+        }
+        else
+        {
+            cur.BitAdd();
+        }
+
+        return cur.ToString();
+    }
+}
+
+public static class StrExtensions
+{
+    public static void BitAdd(this StringBuilder builder)
+    {
+        var ind = builder.Length - 1;
+        while (builder[ind] != '0')
+        {
+            builder[ind] = '0';
+            ind--;
+        }
+
+        builder[ind] = '1';
+    }
+}
---
+++ Archivator/Archivator/Program.cs
@@ -0,0 +1,14 @@
+﻿using Archivator;
+
+var filePath = @"C:\Users\Антон\source\repos\c#\Encoding-and-encryption\Archivator\test.txt";
+var compressedFilePath = @"C:\Users\Антон\source\repos\c#\Encoding-and-encryption\Archivator\compressed";
+var dictFilePath = @"C:\Users\Антон\source\repos\c#\Encoding-and-encryption\Archivator\dict";
+var decompressedFilePath = @"C:\Users\Антон\source\repos\c#\Encoding-and-encryption\Archivator\decompressed";
+
+
+
+
+Haffman.Compress(filePath, dictFilePath, compressedFilePath);
+Haffman.Decompress(dictFilePath, compressedFilePath, decompressedFilePath);
+
+
