c5ff69fa6eeaec03894eeae3121a0356f18f960f
Anton Yaskov <yaskovanton333@gmail.com>
2024-11-23 20:54:00 +0500

    Encryption. Vigener

---
+++ Encryption/Encryption/Program.cs
@@ -0,0 +1,8 @@
+﻿using Encryption.Vigener;
+
+var str = "Метод Array.IndexOf в C# выполняет поиск указанного объекта и возвращает индекс первого найденного совпадения в одномерном массиве или диапазоне элементов массива";
+var key = "ВАСЯ";
+
+var encrypted = Vigener.Encrypt(str, key);
+var decrypted = Vigener.Decrypt(encrypted, key);
+var a = 1;
---
+++ Encryption/Encryption/Vigener/Vigener.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Encryption.Vigener;
+
+public static class Vigener
+{
+    public static string Encrypt(string text, string key)
+    {
+        var shift = GetShift(key);
+        var result = new StringBuilder();
+        for (var i = 0; i < text.Length; i++)
+        {
+            var ch = text[i];
+            var shifted = ch + shift[i % shift.Length];
+            result.Append((char) shifted);
+        }
+
+        return result.ToString();
+    }
+
+    public static string Decrypt(string encrypted, string key)
+    {
+        var shift = GetShift(key);
+        var result = new StringBuilder();
+        for (var i = 0; i < encrypted.Length; i++)
+        {
+            var ch = encrypted[i];
+            var shifted = ch - shift[i % shift.Length];
+            result.Append((char) shifted);
+        }
+
+        return result.ToString();
+    }
+
+    private static int[] GetShift(string key)
+    {
+        var shift = key
+            .Select(ch => (int)ch)
+            .ToArray();
+        
+        return shift;
+    }
+}
